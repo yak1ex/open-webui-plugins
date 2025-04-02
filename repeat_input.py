3cb330f6393e84f2fa78f5bdc8219254a794470d
Yak! <yak1ex@users.noreply.github.com>
2025-04-02 12:05:17 +0900

    [repeat_input.py] Implement 2 IDs handling and a valve to skip input.

--- repeat_input.py
+++ repeat_input.py
@@ -15,19 +15,29 @@ import requests
 import asyncio
 
 from open_webui.utils.misc import get_last_user_message
+from open_webui.env import VERSION
+
+DEFAULT_ONECALL_SUBMIT = tuple(map(int, VERSION.split("."))) >= (0, 5, 21)
 
 class Action:
     class Valves(BaseModel):
-        pass
+        ONECALL_SUBMIT: bool = Field(
+            default=DEFAULT_ONECALL_SUBMIT,
+            description="If true, the action make submit by 1 call. Otherwise, 2 calls are made as required by prior to 0.5.20. Default true for 0.5.21+",
+        )
 
     def __init__(self):
         self.valves = self.Valves()
-        pass
+        self.actions = [
+            {"id": "1-Click"},
+            {"id": "Dialog"}
+        ]
 
     async def action(
         self,
         body: dict,
         __user__=None,
+        __id__=None,
         __event_emitter__=None,
         __event_call__=None,
     ) -> Optional[dict]:
@@ -36,19 +46,19 @@ class Action:
 
         content = get_last_user_message(body["messages"])
 
-        response = await __event_call__(
-            {
-                "type": "input",
-                "data": {
-                    "title": "modify a message",
-                    "message": "here write a message to append",
-                    "value": content,
-                },
-            }
-        )
-        print(response)
+        if __id__ == 'Dialog':
+            content = await __event_call__(
+                {
+                    "type": "input",
+                    "data": {
+                        "title": "modify a message",
+                        "message": "here write a message to append",
+                        "value": content,
+                    },
+                }
+            )
 
-        if response and __event_emitter__:
+        if content and __event_emitter__:
             await __event_emitter__(
                 {
                     "type": "status",
@@ -56,17 +66,18 @@ class Action:
                 }
             )
             await asyncio.sleep(1)
-            response = response.replace("$", "\\$")
+            content = content.replace("$", "\\$")
+            if not self.valves.ONECALL_SUBMIT:
+                await __event_emitter__(
+                    {
+                        "type": "execute",
+                        "data": {"code": f'postMessage({{"type":"input:prompt", "text": String.raw`{content}`}})'}
+                    } 
+                )
             await __event_emitter__(
                 {
                     "type": "execute",
-                    "data": {"code": f'postMessage({{"type":"input:prompt", "text": String.raw`{response}`}})'}
-                } 
-            )
-            await __event_emitter__(
-                {
-                    "type": "execute",
-                    "data": {"code": f'postMessage({{"type":"input:prompt:submit", "text": String.raw`{response}`}})'}
+                    "data": {"code": f'postMessage({{"type":"input:prompt:submit", "text": String.raw`{content}`}})'}
                 } 
             )
             await __event_emitter__(
